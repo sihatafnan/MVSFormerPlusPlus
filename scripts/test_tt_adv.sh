6db3d2b1c316c64c8f084efea6486e1a48d3a8d8
Sihat Afnan <57033114+sihatafnan@users.noreply.github.com>
2025-04-18 21:47:54 -0700

    Update test_tt_adv.sh

--- scripts/test_tt_adv.sh
+++ scripts/test_tt_adv.sh
@@ -1,4 +1,4 @@
-echo Auditorium > /content/MVSFormerPlusPlus/lists/tanksandtemples/advanced.txt && CUDA_VISIBLE_DEVICES=2  python /content/MVSFormerPlusPlus/test_tt.py --dataset tt --batch_size 1 \
+echo Auditorium > /content/MVSFormerPlusPlus/lists/tanksandtemples/advanced.txt && CUDA_VISIBLE_DEVICES=0  python /content/MVSFormerPlusPlus/test_tt.py --dataset tt --batch_size 1 \
  --testpath /content/drive/MyDrive/MVS/tankandtemples/advanced --testlist /content/MVSFormerPlusPlus/lists/tanksandtemples/advanced.txt \
  --config /content/MVSFormerPlusPlus/config/mvsformer++.json \
  --resume /content/drive/MyDrive/MVS/MVSFormer++/model_best.pth    --outdir /content/drive/MyDrive/MVS/MVSFormer++/Outputs  \
