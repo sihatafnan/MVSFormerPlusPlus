f021150e3858edd49353b8ae7b9c72ec49f81d2b
Sihat Afnan <57033114+sihatafnan@users.noreply.github.com>
2025-04-21 15:18:19 -0700

    Update test_tt_adv.sh

--- scripts/test_tt_adv.sh
+++ scripts/test_tt_adv.sh
@@ -5,3 +5,37 @@ echo Auditorium > /content/MVSFormerPlusPlus/lists/tanksandtemples/advanced.txt 
  --interval_scale 1.06 --num_view 5 --fusion_view 5 --numdepth 192 --max_h 864 --max_w 1152 --filter_method dpcd \
  --disp_threshold 0.1   --conf_choose mean --conf 0.3
 
+echo Ballroom > /content/MVSFormerPlusPlus/lists/tanksandtemples/advanced.txt && CUDA_VISIBLE_DEVICES=0  python /content/MVSFormerPlusPlus/test_tt.py --dataset tt --batch_size 1 \
+ --testpath /content/drive/MyDrive/MVS/tankandtemples/advanced --testlist /content/MVSFormerPlusPlus/lists/tanksandtemples/advanced.txt \
+ --config /content/MVSFormerPlusPlus/config/mvsformer++.json \
+ --resume /content/drive/MyDrive/MVS/MVSFormer++/model_best.pth    --outdir /content/drive/MyDrive/MVS/MVSFormer++/Outputs  \
+ --interval_scale 1.06 --num_view 5 --fusion_view 5 --numdepth 192 --max_h 864 --max_w 1152 --filter_method dpcd \
+ --disp_threshold 0.1   --conf_choose mean --conf 0.3
+
+ echo Courtroom > /content/MVSFormerPlusPlus/lists/tanksandtemples/advanced.txt && CUDA_VISIBLE_DEVICES=0  python /content/MVSFormerPlusPlus/test_tt.py --dataset tt --batch_size 1 \
+ --testpath /content/drive/MyDrive/MVS/tankandtemples/advanced --testlist /content/MVSFormerPlusPlus/lists/tanksandtemples/advanced.txt \
+ --config /content/MVSFormerPlusPlus/config/mvsformer++.json \
+ --resume /content/drive/MyDrive/MVS/MVSFormer++/model_best.pth    --outdir /content/drive/MyDrive/MVS/MVSFormer++/Outputs  \
+ --interval_scale 1.06 --num_view 5 --fusion_view 5 --numdepth 192 --max_h 864 --max_w 1152 --filter_method dpcd \
+ --disp_threshold 0.1   --conf_choose mean --conf 0.3
+
+ echo Museum > /content/MVSFormerPlusPlus/lists/tanksandtemples/advanced.txt && CUDA_VISIBLE_DEVICES=0  python /content/MVSFormerPlusPlus/test_tt.py --dataset tt --batch_size 1 \
+ --testpath /content/drive/MyDrive/MVS/tankandtemples/advanced --testlist /content/MVSFormerPlusPlus/lists/tanksandtemples/advanced.txt \
+ --config /content/MVSFormerPlusPlus/config/mvsformer++.json \
+ --resume /content/drive/MyDrive/MVS/MVSFormer++/model_best.pth    --outdir /content/drive/MyDrive/MVS/MVSFormer++/Outputs  \
+ --interval_scale 1.06 --num_view 5 --fusion_view 5 --numdepth 192 --max_h 864 --max_w 1152 --filter_method dpcd \
+ --disp_threshold 0.1   --conf_choose mean --conf 0.3
+
+ echo Palace > /content/MVSFormerPlusPlus/lists/tanksandtemples/advanced.txt && CUDA_VISIBLE_DEVICES=0  python /content/MVSFormerPlusPlus/test_tt.py --dataset tt --batch_size 1 \
+ --testpath /content/drive/MyDrive/MVS/tankandtemples/advanced --testlist /content/MVSFormerPlusPlus/lists/tanksandtemples/advanced.txt \
+ --config /content/MVSFormerPlusPlus/config/mvsformer++.json \
+ --resume /content/drive/MyDrive/MVS/MVSFormer++/model_best.pth    --outdir /content/drive/MyDrive/MVS/MVSFormer++/Outputs  \
+ --interval_scale 1.06 --num_view 5 --fusion_view 5 --numdepth 192 --max_h 864 --max_w 1152 --filter_method dpcd \
+ --disp_threshold 0.1   --conf_choose mean --conf 0.3
+
+ echo Temple > /content/MVSFormerPlusPlus/lists/tanksandtemples/advanced.txt && CUDA_VISIBLE_DEVICES=0  python /content/MVSFormerPlusPlus/test_tt.py --dataset tt --batch_size 1 \
+ --testpath /content/drive/MyDrive/MVS/tankandtemples/advanced --testlist /content/MVSFormerPlusPlus/lists/tanksandtemples/advanced.txt \
+ --config /content/MVSFormerPlusPlus/config/mvsformer++.json \
+ --resume /content/drive/MyDrive/MVS/MVSFormer++/model_best.pth    --outdir /content/drive/MyDrive/MVS/MVSFormer++/Outputs  \
+ --interval_scale 1.06 --num_view 5 --fusion_view 5 --numdepth 192 --max_h 864 --max_w 1152 --filter_method dpcd \
+ --disp_threshold 0.1   --conf_choose mean --conf 0.3
